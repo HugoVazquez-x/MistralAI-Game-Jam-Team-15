4b66cb152d606aa72c76e03d3d3205f5730dba4a
Louis Martinez <louis.martinez@telecom-paris.fr>
2025-01-25 15:19:57 +0100

    Added intial context generation

--- src/hackathon/game_mechanics/pre_game_mechanics.py
+++ src/hackathon/game_mechanics/pre_game_mechanics.py
@@ -5,7 +5,23 @@ from typing import Tuple, List
 import hackathon.game_mechanics.entities as entities
 
 
+def generate_background_personality(character: ch.AIAgent, client):
+    initial_context = character.general_context
 
+    message = f"Here is the background story of a political personality. Replace simlessly missing values identified by the <TO_FILL> tag with some realistic information. All the facts don't have to be truse but they must make sense with the rest of the personality of the character. Don't make the added text appear in bold. Absolutely avoid filling holes with too controversial ideas: {initial_context}"
+    
+    chat_response = self.client.chat.complete(
+        model=character.model,
+        messages=[
+            {
+                "role": "user",
+                "content": message
+            }
+        ]
+    )
+    # ------------------------------------------------
+    character.general_context = chat_response.choices[0].message.content
+    
 
 def add_cards_to_personal_context(card_agent:ar.CardAgent, characters: List[ch.AIAgent], cards:entities.Card):
     for character in characters:
